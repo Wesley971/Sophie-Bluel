6b7f03a280b53c6358b25ca6da257fe2a840f640
Wesley Abdoul <Abdoulwes@gmail.com>
2024-01-16 18:11:16 +0100

    Optimisation

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,113 +1,126 @@
 
+// Sélectionne les éléments nécessaires dans le DOM
 const gallery = document.querySelector(".gallery");
 const filters = document.querySelector(".filters");
 
+// Charge les projets à partir de l'API
 async function loadProjects() {
-    const response = await fetch("http://localhost:5678/api/works");
-    return await response.json();
+   const response = await fetch("http://localhost:5678/api/works");
+   return await response.json();
 }
 
+// Crée un élément de projet à partir d'un objet photo
 function createProjectElement(photo) {
-    const showPicture = document.createElement("figure");
-    const showTitle = document.createElement("figcaption");
-    const showImg = document.createElement("img");
+   const showPicture = document.createElement("figure");
+   const showTitle = document.createElement("figcaption");
+   const showImg = document.createElement("img");
 
-    showImg.src = photo.imageUrl;
-    showTitle.innerText = photo.title;
+   showImg.src = photo.imageUrl;
+   showTitle.innerText = photo.title;
 
-    showPicture.appendChild(showImg);
-    showPicture.appendChild(showTitle);
-    gallery.appendChild(showPicture);
+   showPicture.appendChild(showImg);
+   showPicture.appendChild(showTitle);
+   gallery.appendChild(showPicture);
 }
 
+// Affiche tous les projets
 async function showProjects() {
-    const projects = await loadProjects();
-    projects.forEach(createProjectElement);
+   const projects = await loadProjects();
+   projects.forEach(createProjectElement);
 }
 
+// Charge les catégories à partir de l'API
 async function loadCategories() {
-    const response = await fetch("http://localhost:5678/api/categories");
-    return await response.json();
+   const response = await fetch("http://localhost:5678/api/categories");
+   return await response.json();
 }
 
+// Affiche les boutons de filtrage des catégories
 async function showCategoriesButtons() {
-    const categories = await loadCategories();
+   const categories = await loadCategories();
 
-    categories.forEach((group) => {
-        const button = document.createElement("button");
-        button.textContent = group.name;
-        button.id = group.id;
-        filters.appendChild(button);
-    });
+   categories.forEach((group) => {
+       const button = document.createElement("button");
+       button.textContent = group.name;
+       button.id = group.id;
+       filters.appendChild(button);
+   });
 }
 
+// Filtre les projets par catégorie lorsqu'un bouton de catégorie est cliqué
 async function filterCategories() {
-    const projects = await loadProjects();
-    const buttons = document.querySelectorAll(".filters button");
+   const projects = await loadProjects();
+   const buttons = document.querySelectorAll(".filters button");
 
-    buttons.forEach((button) => {
-        button.addEventListener("click", async (event) => {
-            const btnId = event.target.id;
-            gallery.innerHTML = "";
+   buttons.forEach((button) => {
+       button.addEventListener("click", async (event) => {
+           const btnId = event.target.id;
+           gallery.innerHTML = "";
 
-            if (btnId !== "0") {
-                const projectCategories = projects.filter((project) => project.categoryId == btnId);
-                projectCategories.forEach(createProjectElement);
-            } else {
-                await showProjects();
-            }
-        });
-    });
+           if (btnId !== "0") {
+               const projectCategories = projects.filter((project) => project.categoryId == btnId);
+               projectCategories.forEach(createProjectElement);
+           } else {
+               await showProjects();
+           }
+       });
+   });
 }
 
+// Initialise l'affichage des projets et des boutons de filtrage
 async function init() {
-    await showProjects();
-    await showCategoriesButtons();
-    filterCategories();
+   await showProjects();
+   await showCategoriesButtons();
+   filterCategories();
 }
 
 init();
 
+// Vérifie le statut de connexion de l'utilisateur
 function checkLoginStatus() {
-    const token = sessionStorage.getItem("token");
-    const loginButton = document.getElementById("loginButton");
-    const logoutButton = document.getElementById("logoutButton");
-    const modifySection = document.getElementById("modifySection");
-    const filters = document.querySelector(".filters");
+   const token = sessionStorage.getItem("token");
+   const loginButton = document.getElementById("loginButton");
+   const logoutButton = document.getElementById("logoutButton");
+   const modifySection = document.getElementById("modifySection");
+   const filters = document.querySelector(".filters");
 
-    if (token) {
-        loginButton.classList.add("hidden");
-        logoutButton.classList.remove("hidden");
+   if (token) {
+       loginButton.classList.add("hidden");
+       logoutButton.classList.remove("hidden");
 
-        if (filters) {
-            filters.classList.add("hidden");
-        }
+       if (filters) {
+           filters.classList.add("hidden");
+       }
 
-        modifySection.classList.remove("hidden");
-        logoutButton.addEventListener("click", logout);
-    } else {
-        loginButton.classList.remove("hidden");
-        logoutButton.classList.add("hidden");
+       modifySection.classList.remove("hidden");
+       logoutButton.addEventListener("click", logout);
+   } else {
+       loginButton.classList.remove("hidden");
+       logoutButton.classList.add("hidden");
 
-        if (filters) {
-            filters.classList.remove("hidden");
-        }
+       if (filters) {
+           filters.classList.remove("hidden");
+       }
 
-        modifySection.classList.add("hidden");
-        loginButton.addEventListener("click", redirectToLoginPage);
-    }
+       modifySection.classList.add("hidden");
+       loginButton.addEventListener("click", redirectToLoginPage);
+   }
 }
 
+// Redirige l'utilisateur vers la page de connexion
 function redirectToLoginPage() {
-    window.location.href = "login.html";
+   window.location.href = "login.html";
 }
 
+// Déconnecte l'utilisateur
 function logout() {
-    sessionStorage.removeItem("token");
-    sessionStorage.removeItem("userId");
-    window.location.href = "index.html";
+   sessionStorage.removeItem("token");
+   sessionStorage.removeItem("userId");
+   window.location.href = "index.html";
 }
 
+// Vérifie le statut de connexion de l'utilisateur au chargement de la page
 checkLoginStatus();
+
 
 
--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,18 +1,17 @@
-// Variables globales
-const modifySection = document.getElementById("modifySection");
-const closeModalButton = document.getElementById("closeModalButton");
-const addPhotoButton = document.getElementById("addPhotoButton");
-const modalContainer = document.querySelector(".containeModals");
-const projectsModal = document.querySelector(".projectsModal");
-
 document.addEventListener("DOMContentLoaded", function () {
-
+    // Variables globales
+    const modifySection = document.getElementById("modifySection");
+    const closeModalButton = document.getElementById("closeModalButton");
+    const addPhotoButton = document.getElementById("addPhotoButton");
+    const modalContainer = document.querySelector(".containeModals");
+    const projectsModal = document.querySelector(".projectsModal");
+ 
     // Fonction pour vérifier si l'utilisateur est connecté
     function isLoggedIn() {
         const token = sessionStorage.getItem("token");
         return !!token;
     }
-
+ 
     // Fonction pour ouvrir la modale
     function openModal() {
         if (isLoggedIn()) {
@@ -22,25 +21,30 @@ document.addEventListener("DOMContentLoaded", function () {
             redirectToLoginPage();
         }
     }
-
+ 
     // Fonction pour fermer la modale
     function closeModal() {
         modalContainer.style.display = "none";
         document.querySelector('.overlay').style.display = 'none';
     }
-
+ 
+    // Fonction pour rediriger vers la page de connexion
+    function redirectToLoginPage() {
+        window.location.href = "login.html";
+    }
+ 
+    // Ajoutez des écouteurs d'événements
     modifySection.addEventListener("click", openModal);
-
-    // Ajoutez un écouteur d'événements pour l'icône
+ 
     const modifyIcon = document.querySelector(".fa-pen-to-square");
     if (modifyIcon) {
         modifyIcon.addEventListener("click", openModal);
     }
-
+ 
     closeModalButton.addEventListener("click", () => {
         closeModal();
     });
-
+ 
     addPhotoButton.addEventListener("click", () => {
         if (isLoggedIn()) {
             // Ajoutez ici le code pour gérer l'ajout de photo
@@ -49,47 +53,43 @@ document.addEventListener("DOMContentLoaded", function () {
             redirectToLoginPage();
         }
     });
-
-    function redirectToLoginPage() {
-        window.location.href = "login.html";
-    }
-});
-
-async function displayProjectsModal() {
+ });
+ 
+ async function displayProjectsModal() {
     const projectsModal = document.querySelector(".projectsModal");
     projectsModal.innerHTML = "";
-
+ 
     const projects = await loadProjects();
-
+ 
     projects.forEach(project => {
         const figure = document.createElement("figure");
         const img = document.createElement("img");
         const span = document.createElement("span");
         const del = document.createElement("i");
-
+ 
         del.classList.add("fas", "fa-trash-can");
         del.id = project.id;
-
+ 
         img.src = project.imageUrl;
-
+ 
         span.appendChild(del);
         figure.appendChild(span);
         figure.appendChild(img);
-
+ 
         projectsModal.appendChild(figure);
     });
-
+ 
     deleteProjects(); // Ajoutez cet appel pour initialiser les événements de suppression
-}
-
-displayProjectsModal();
-
-function deleteProject(id) {
+ }
+ 
+ displayProjectsModal();
+ 
+ function deleteProject(id) {
     const init = {
         method: "DELETE",
-        headers: { "Content-Type": "application/json", "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4" }
+        headers: { "Content-Type": "application/json", "Authorization": "Bearer " + sessionStorage.getItem("token") } 
     };
-
+    
     fetch(`http://localhost:5678/api/works/${id}`, init)
         .then(response => {
             if (!response.ok) {
@@ -107,9 +107,9 @@ function deleteProject(id) {
         .catch(error => {
             console.error("Erreur lors de la suppression :", error);
         });
-}
-
-function deleteProjects() {
+ }
+ 
+ function deleteProjects() {
     const deleteAll = document.querySelectorAll(".fa-trash-can");
     deleteAll.forEach(deleteItem => {
         deleteItem.addEventListener("click", (event) => {
@@ -117,4 +117,5 @@ function deleteProjects() {
             deleteProject(id);
         });
     });
-}
+ }
+ 
